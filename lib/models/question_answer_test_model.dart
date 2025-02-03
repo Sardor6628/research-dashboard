a36f4b94e6f283f67c6be838d9cc9a691f0e1f0a
Sardor6628 <sardor.dsu@gmail.com>
2025-02-03 16:53:21 +0900

    QA  test model

---
+++ lib/models/question_answer_test_model.dart
@@ -0,0 +1,28 @@
+class QuestionAnswerTestModel {
+  final String user;
+  final String question;
+  final bool isExistOnDb;
+  final String questionOnDb;
+  final String answer;
+  final String answerFrom;
+
+  QuestionAnswerTestModel({
+    required this.user,
+    required this.question,
+    required this.isExistOnDb,
+    required this.questionOnDb,
+    required this.answer,
+    required this.answerFrom,
+  });
+
+  factory QuestionAnswerTestModel.fromJson(Map<String, dynamic> json) {
+    return QuestionAnswerTestModel(
+      user: json['user'],
+      question: json['question'],
+      isExistOnDb: json['is_exist_on_db'],
+      questionOnDb: json['question_on_db'] ?? "",
+      answer: json['answer'],
+      answerFrom: json['answer_from'],
+    );
+  }
+}
